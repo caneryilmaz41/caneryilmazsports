53502de24e9cef34d6220acdf7b9c8113db09265
Caner YILMAZ <beat10tr@gmail.com>
2025-08-30 16:16:10 +0300

    adfsrggt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react";
 import Hls from "hls.js";
-export const BASE_URL = import.meta.env.SERVER_URL || "http://localhost:5001"
+export const BASE_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:5001"
 
 /* Hafif, bağımsız ikonlar (kütüphane yok, inline SVG) */
 const IconBall = (props) => (

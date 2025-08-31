39bd42d2001cd028a5277448327f684e92f2dcf9
Caner YILMAZ <beat10tr@gmail.com>
2025-08-31 18:23:24 +0300

    fsgfsgsf

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useMemo, useState } from "react";
 import Hls from "hls.js";
 
 const getServerUrl = () => {
-  const url = process.env.REACT_APP_SERVER_URL || "https://caneryilmazsports.onrender.com";
+  const url = process.env.REACT_APP_SERVER_URL || "https://caneryilmazsports-backend.onrender.com";
   console.log('Server URL:', url);
   return url;
 };
